832e49d44401417eace2fc3d2fbd3fee6aebadd7
Kamal Lama <kamal09rumba@gmail.com>
2022-01-10 17:16:20 +0545

    Add github actions, dependabot and pre-commit hooks

--- lukimgather/wsgi.py
+++ lukimgather/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lukimgather.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lukimgather.settings")
 
 application = get_wsgi_application()
