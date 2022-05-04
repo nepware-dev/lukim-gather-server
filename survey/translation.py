ce9beca2d87b406259d051cb0beacc67f51c21b1
Kamal Lama <kamal09rumba@gmail.com>
2022-05-04 11:57:40 +0545

    Change survey form

--- survey/translation.py
+++ survey/translation.py
@@ -1,23 +1,8 @@
 from modeltranslation import translator
 
-from survey.models import Option, ProtectedAreaCategory, Question, QuestionGroup
+from survey.models import ProtectedAreaCategory
 
 
 @translator.register(ProtectedAreaCategory)
 class AreaCategoryTranslationOptions(translator.TranslationOptions):
-    fields = ("title",)
-
-
-@translator.register(QuestionGroup)
-class QuestionGroupTranslationOptions(translator.TranslationOptions):
-    fields = ("title",)
-
-
-@translator.register(Question)
-class QuestionTranslationOptions(translator.TranslationOptions):
-    fields = ("title", "description", "hints")
-
-
-@translator.register(Option)
-class OptionTranslationOptions(translator.TranslationOptions):
     fields = ("title",)
