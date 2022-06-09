3a19b70a9377f36ca78daa90038d7bba86de5a6b
Kamal Lama <kamal09rumba@gmail.com>
2022-06-09 11:07:34 +0545

    Add vector tiles for happening survey

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.gis.db.models import GeometryField
+from django.db.models.functions import Coalesce
+from django.views.generic import ListView
+from vectortiles.postgis.views import MVTView
 
-# Create your views here.
+from survey.models import HappeningSurvey
+
+
+class TileView(MVTView, ListView):
+    model = HappeningSurvey
+    vector_tile_layer_name = "happening-surveys"
+    vector_tile_fields = (
+        "id",
+        "category__title",
+        "title",
+        "description",
+        "sentiment",
+        "status",
+        "improvement",
+    )
+    vector_tile_geom_name = "geom"
+    vector_tile_queryset = HappeningSurvey.objects.filter(is_public=True).annotate(
+        geom=Coalesce("location", "boundary", output_field=GeometryField(srid=4326))
+    )
