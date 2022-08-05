b11880c60ee871232f243ba1818d4f7645cf6e2d
Kamal Lama <kamal09rumba@gmail.com>
2022-08-05 10:52:34 +0545

    Add protected area filter in survey

--- region/schema.py
+++ region/schema.py
@@ -1,11 +1,16 @@
 import graphene
 from graphene_django_extras import DjangoFilterPaginateListField
 
-from .filters import RegionFilter
-from .types import RegionType
+from .filters import ProtectedAreaFilter, RegionFilter
+from .types import ProtectedAreaType, RegionType
 
 
 class RegionQueries(graphene.ObjectType):
     regions = DjangoFilterPaginateListField(
         RegionType, description="Returns regions", filterset_class=RegionFilter
+    )
+    protected_areas = DjangoFilterPaginateListField(
+        ProtectedAreaType,
+        description="Returns protected areas",
+        filterset_class=ProtectedAreaFilter,
     )
