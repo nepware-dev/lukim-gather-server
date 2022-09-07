08692b98782e38f6e2ee3e822dc11af89877edee
Kamal Lama <kamal09rumba@gmail.com>
2022-09-07 16:56:10 +0545

    Fix announcement message

--- notification/signals.py
+++ notification/signals.py
@@ -1,6 +1,7 @@
 from celery.utils.imports import instantiate
 from django.db.models.signals import m2m_changed, post_save
 from django.dispatch.dispatcher import receiver
+from django.utils.html import strip_tags
 
 from support.models import EmailTemplate
 from user.models import User
@@ -13,11 +14,6 @@ from .models import Announcement
 @receiver(post_save, sender=Announcement)
 def send_announcement(sender, instance, **kwargs):
     users = []
-    subject, html_message, text_message = EmailTemplate.objects.get(
-        identifier="announcement"
-    ).get_email_contents(
-        {"user": instance, "announcement_object": instance.description}
-    )
     if "notification.Announcement_organization" in sender._meta.label:
         organizations = instance.organization.all()
         for organization in organizations:
@@ -28,7 +24,7 @@ def send_announcement(sender, instance, **kwargs):
                     instance.title,
                     action_object=instance,
                     notification_type="announcement",
-                    description=instance.description,
+                    description=strip_tags(instance.description),
                 )
     elif "notification.Announcement_user" in sender._meta.label or instance.notify_all:
         users = User.objects.all() if instance.notify_all else instance.user.all()
@@ -38,11 +34,21 @@ def send_announcement(sender, instance, **kwargs):
                 instance.title,
                 action_object=instance,
                 notification_type="announcement",
-                description=instance.description,
+                description=strip_tags(instance.description),
             )
             if not instance.notify_all:
+                subject, html_message, text_message = EmailTemplate.objects.get(
+                    identifier="announcement"
+                ).get_email_contents(
+                    {
+                        "user": user,
+                        "announcement_object": strip_tags(instance.description),
+                    }
+                )
                 if user.phone_number:
-                    user.celery_sms_user(to=user.phone_number, message=text_message)
+                    user.celery_sms_user(
+                        to=user.phone_number.as_international, message=text_message
+                    )
                 elif user.email:
                     user.celery_email_user(
                         subject, text_message, html_message=html_message
